84386316eeee010e5988d576de5307c4dca45b4f
Thiago Viana Bezerra <Thiagovb62@gmail.com>
2022-05-17 15:27:51 -0300

    Introduçao a classes em js

--- Classes_Js/aula-1/index.js
+++ Classes_Js/aula-1/index.js
@@ -1,1 +1,24 @@
-console.log();
+class Pessoa {
+  constructor(nome, sobrenome) {
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+  }
+
+  falar() {
+    console.log(this.nome + " esta falando!");
+  }
+  comer() {
+    console.log(this.nome + "esta comendo");
+  }
+}
+function Pessoa2(nome, sobrenome) {
+  this.nome = nome;
+  this.sobrenome = sobrenome;
+}
+Pessoa2.prototype.falar = function () {
+  console.log(this.nome + " esta falando!");
+};
+const p1 = new Pessoa("Luiz", "Miranda");
+const p2 = new Pessoa2("Mario", "Miranda");
+p1.falar();
+p2.falar();
